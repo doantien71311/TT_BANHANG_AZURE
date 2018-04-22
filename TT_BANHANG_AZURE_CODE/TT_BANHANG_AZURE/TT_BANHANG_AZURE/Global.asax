356269deffe7448b7d28ae715a90a9c7fd010fdf
Bui Tien <mrtien71311@gmail.com>
2018-04-22 17:49:11 +0700

    220418
    Fist commit

---
+++ TT_BANHANG_AZURE_CODE/TT_BANHANG_AZURE/TT_BANHANG_AZURE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TT_BANHANG_AZURE.MvcApplication" Language="C#" %>
---
+++ TT_BANHANG_AZURE_CODE/TT_BANHANG_AZURE/TT_BANHANG_AZURE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TT_BANHANG_AZURE.Startup))]
+namespace TT_BANHANG_AZURE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
